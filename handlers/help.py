f0b70d31191d5570f17cd8654e2859910cfa2211
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-07-27 19:27:04 -0700

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -37,7 +37,7 @@ async def help_cmd(client: Client, message: Message):
                         "💫 sᴜᴩᴩᴏʀᴛ 💫", url=f"https://t.me/{SUPPORT_GROUP}"
                     ),
                     InlineKeyboardButton(
-                        "❄ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ ❄", url="https://github.com/harshparmar0/TBHVCBOT"
+                        "❄ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ ❄", url="https://github.com/MrProgrammer72/GJ516VCBOT"
                     )
                 ],
                 [
