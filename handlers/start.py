1ff97f84dbe9f9bedba5b8ce45c5bad828bba03f
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-08-20 09:57:52 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -43,7 +43,7 @@ async def start_(client: Client, message: Message):
                         "🔎 ɪɴʟɪɴᴇ 🔎", switch_inline_query_current_chat=""
                     ),
                     InlineKeyboardButton(
-                        "🤯 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ 🤯", url="https://telegra.ph/file/9b0455dae14d5639f936d.mp4"
+                        "💡Git repo💡", url="https://telegra.ph/file/9b0455dae14d5639f936d.mp4"
                     )]
             ]
        ),
