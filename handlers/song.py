e44fae7cff11aa51d8217c4fe3c6f45cd3056ddc
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-09-16 07:11:47 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -63,7 +63,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit("** Downloading error, report this at  [{bn} support](t.me/{bu}) ✨**")
+        m.edit("** Downloading error, report this at  [{bn} support](t.me/{bu}) **")
         print(e)
 
     try:
