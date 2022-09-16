c00fec65b541f4e3765ffa5955161d57198b5092
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-09-16 06:38:29 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -21,7 +21,7 @@ async def ytsearch(_, message: Message):
     await message.delete()
     try:
         if len(message.command) < 2:
-            await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ʙᴀʙʏ!")
+            await message.reply_text(" Give some text to search!")
             return
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("🔎")
@@ -29,11 +29,11 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"📌 ᴛɪᴛʟᴇ : {results[i]['title']}\n"
-            text += f"⏱ ᴅᴜʀᴀᴛɪᴏɴ : {results[i]['duration']}\n"
-            text += f"👀 ᴠɪᴇᴡs : {results[i]['views']}\n"
-            text += f"📣 ᴄʜᴀɴɴᴇʟ : {results[i]['channel']}\n"
-            text += f"🔗 ʟɪɴᴋ : https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"📌 Title : {results[i]['title']}\n"
+            text += f"⏱ duration : {results[i]['duration']}\n"
+            text += f"👀 views : {results[i]['views']}\n"
+            text += f"📣 channel : {results[i]['channel']}\n"
+            text += f"🔗 link : https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
