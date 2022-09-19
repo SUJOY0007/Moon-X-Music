a9e6a4276f1fdb1a43e4eb12fc29f0d8cf17ef90
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-09-19 08:30:00 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from helpers.filters import command
-from config import BOT_NAME as bn, BOT_USERNAME as bu, SUPPORT_GROUP, OWNER_USERNAME, CHANNEL_UPDATES as me, START_IMG
+from config import BOT_NAME as bn, BOT_USERNAME as bu, CHANNEL_UPDATES, SUPPORT_GROUP, OWNER_USERNAME as me, START_IMG
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
