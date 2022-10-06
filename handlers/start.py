8d6138ff03e044c8bf68f6ed5aa388f56e153bc5
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-10-06 10:14:24 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -11,7 +11,7 @@ async def start_(client: Client, message: Message):
     await message.reply_sticker("CAACAgIAAx0CZD3aQwACJ_hjPkS4Komyh2BnI4s_NUdAdzYSpwAClgAD5KDOB-VgUnOaX5eTKgQ")
     await message.reply_photo(
         photo=f"{START_IMG}",
-        caption=f""" **Hey {message.from_user.mention()},🖤**\n   **This is [{bn}](t.me/{bu}), ** 🥀\n\n** ⦿ A powerfull music player bot with some awesome and useful features.**\n** ⦿ All of my commands are (/ . • $ ^ ~ + * ?).\n ⦿ Managed 🖤 by: Jay🥀** """,
+        caption=f""" **Hey {message.from_user.mention()},🖤**\n   **This is [{bn}](t.me/{bu}), ** 🥀\n\n** ⦿ A powerfull music player bot with some awesome and useful features.**\n** ⦿ All of my commands are (/ . • $ ^ ~ + * ?).\n ⦿ Made 🖤 by: [𝗝𝝙𝗬 ](https://t.me/export_gabbar)** """,
     reply_markup=InlineKeyboardMarkup(
             [
                 [
