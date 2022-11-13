393193f520c8808acdc7d407c74ec064a12385b2
Rajpomane <95400073+Rajpomane@users.noreply.github.com>
2022-11-13 11:23:04 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -8,42 +8,48 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
 async def start_(client: Client, message: Message):
-    await message.reply_sticker("CAACAgUAAx0CZp7eIAACYn9jQ4fSHUbGxAMFJz_-kaRcOuoRWAACDgcAAruXGFbarx8_grqJYh4E")
+    await message.reply_sticker("tg://openmessage?user_id=5564920980&message_id=53661")
     await message.reply_photo(
         photo=f"{START_IMG}",
-        caption=f""" ** Hey {message.from_user.mention()} , 🥀\n\n
+        caption=f""" ** Hey {message.from_user.mention()} , ⚔️\n\n
 ๏ This is [{bn}](t.me/{bu}) ,  !
-➻ The most Powerful telegram music  bot with some awesome and useful features.
+➻ 👑Official Account🖤
+💟Wish Me On 25 July 🎂
+⚡My Life My Rules💪
+🎶Music ka Diwana💥
+🕉️Mahadev Bhakt🕉️
+♍I’m not Rich ßut I’m Royal 👑
+༒︎Iɴsᴛᴀɢʀᴀᴍ ɪᴅ ༒︎k_i_n_g_o_f_d_e_v_i_l_s_0_0_7 ☠︎︎.
 
 ──────────────────
 ๏  All of my command can be used with My command handle : ( / . • $ ^ ~ + * ? )
-➻ Made 🖤 by : [𝗝𝝙𝗬🥀](https://t.me/{me}) ** """,
+➻ Made 🫶🏻 by : [𝐇𝐀𝐂𝐊𝐄𝐑❤️‍🔥](https://t.me/{me}) ** """,
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "✚ Add me to your Group", url=f"https://t.me/{bu}?startgroup=true"
+                        "✚ group me add kar re ", url=f"https://t.me/{bu}?startgroup=true"
                        ),
                  ],[
                     InlineKeyboardButton(
-                        "📨 Channel ", url=f"https://t.me/{CHANNEL_UPDATES}"
+                        "⚔️ Chal Channel ko support kar  ", url=f"https://t.me/{CHANNEL_UPDATES}"
                     ),
                     InlineKeyboardButton(
-                        "📨 Support ", url=f"https://t.me/{SUPPORT_GROUP}"
+                        "⚔️ Group Support ", url=f"https://t.me/{SUPPORT_GROUP}"
                     )
                   ],[
                     InlineKeyboardButton(
-                        "👤 Bot Owner ", url=f"https://t.me/{me}"
+                        "👤 King owner ", url=f"https://t.me/{me}"
                     ),
                     InlineKeyboardButton(
-                        "👨‍💻 Developer ", url=f"https://t.me/export_gabbar"
+                        "👨‍💻 Developer ", url=f"https://t.me/RAJPUT_KA_CHORA"
                     ),
                   ],[
                     InlineKeyboardButton(
                         "✅ Inline ", switch_inline_query_current_chat=""
                     ),
                     InlineKeyboardButton(
-                        "💡 Git repo", url="https://github.com/MrProgrammer72/GJ516VCBOT"
+                        "💡 Git repo", url="https://telegra.ph/file/089864f9401b686c645cb.mp4"
                     )]
             ]
        ),
